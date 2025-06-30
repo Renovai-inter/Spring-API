46136e69763aad8979e37d09deac15b7f9f22a64
iago511 <iagonsiodiniz@gmail.com>
2025-06-30 10:40:00 -0300

    configurei o banco de dados e as properties principais

---
+++ src/main/java/com/renovai/api/RenovaiApplication.java
@@ -0,0 +1,11 @@
+package com.renovai.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovaiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RenovaiApplication.class, args);
+    }
+}
