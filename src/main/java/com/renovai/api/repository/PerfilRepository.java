e8eca842084867213a3eacfa96c69e47022f1b31
iago511 <iagonsiodiniz@gmail.com>
2025-07-02 09:30:00 -0300

    repositories de usuario perfil e endereco

---
+++ src/main/java/com/renovai/api/repository/PerfilRepository.java
@@ -0,0 +1,14 @@
+package com.renovai.api.repository;
+
+import com.renovai.api.model.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+import java.util.List;
+
+@Repository
+public interface PerfilRepository extends JpaRepository<Perfil, Integer> {
+    Optional<Perfil> findByEmail(String email);
+    boolean existsByEmail(String email);
+    List<Perfil> findByEstaAtivoTrue();
+}
---
+++ src/main/java/com/renovai/api/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.renovai.api.repository;
+
+import com.renovai.api.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByCpf(String cpf);
+    boolean existsByCpf(String cpf);
+}
